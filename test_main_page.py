6e150de5305c955d62dd88730fc36069aa4140ab
larnaa <larnaa@localhost.localdomain>
2019-10-29 15:54:43 +0300

    Added method go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,12 @@
 LINK = 'http://selenium1py.pythonanywhere.com/'
 
 
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+
 def test_guest_can_go_to_login_page(browser):
     browser.get(LINK)
-    login_link = browser.find_element_by_css_selector('#login_link')
-    login_link.click()
+    go_to_login_page(browser)
+
