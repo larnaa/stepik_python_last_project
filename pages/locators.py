d098a59201b6d06d3ced1f39bd59a4bd875bb71f
larnaa <larnaa@localhost.localdomain>
2019-11-07 14:02:25 +0300

    Added locators: login and register forms

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,19 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
+
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators():
+
+    LOGIN_FORM = (By.ID, 'logi_form')
+#    LOGIN_EMAIL_FIELD = (By.ID, 'id_login-username')
+#    LOGIN_PASSWORD_FIELD = (By.ID, 'id_login-password')
+
+    REGISTER_FORM = (By.ID, 'registe_form')
+#    REGISTER_EMAIL_FIELD = (By.ID, 'id_registration-email')
+#    REGISTER_PASSWORD_FIELD = (By.ID, 'id_registration-password1')
+#    REGISTER_CONFIRM_PASSWORD_FIELD = (By.ID, 'id_registration-password2')
+
+
